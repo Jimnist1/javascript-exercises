7bb256fd0efecca00f45f93e6669c500aa9329cf
James Wimmers <jimmyskydive@gmail.com>
2023-06-18 14:09:10 +1200

    finished excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (word, numberRepeats) {
+  if (numberRepeats >= 0) {
+    let repeatedWord = "";
+    while (numberRepeats > 0) {
+      repeatedWord += word;
+      numberRepeats--;
+    }
+    return repeatedWord;
+  } else return "ERROR";
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
