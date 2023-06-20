96f692b8cf1e2d92385b97f1ffe7f59eeb8e84b9
James Wimmers <jimmyskydive@gmail.com>
2023-06-20 22:02:15 +1200

    Excercise complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let i = string.length;
+  let reversedString = [];
+  while (i >= 0) {
+    let slicedString = string.slice(i - 1, i);
+    reversedString.push(slicedString);
+    i--;
+  }
+  return reversedString.join("");
 };
-
+reverseString("Hello");
 // Do not edit below this line
 module.exports = reverseString;
